8673151a133f1d06071d6d1c46f0092d9216a740
Byron Clarke <bynaclarke@gmail.com>
2026-09-07 14:04:45 +0200

    removed clutter

--- work/54/98ab74f314dd91985d0d04872cb689/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir test
-python3 make_distance_matrix.py -i rusty_metal_test.csv -o test
--- work/ba/68d7ebd8fa8cac11cc93d088a3c6a5/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-rusty-metal kalign.fasta muscle.fasta t_coffee.fasta mafft_linsi.fasta mafft_auto.fasta -o rusty_metal_test.csv
