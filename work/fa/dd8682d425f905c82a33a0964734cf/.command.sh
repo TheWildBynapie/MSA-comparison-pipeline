a7a221321116edc610c42bff221bf2bf8c121fe2
Byron Clarke <bynaclarke@gmail.com>
2026-09-07 12:27:42 +0200

    removed temp files

--- work/fa/dd8682d425f905c82a33a0964734cf/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-rusty-metal fsa.fasta clustalo.fasta probcons.fasta muscle.fasta amap.fasta kalign.fasta clustalw.fasta t_coffee.fasta mafft.fasta prank.fasta -o rusty_metal_test_gapless.csv
